10dfa7e7f7e2019e778a1683afc18b0c455da1f1
NamanDhariwal <114096680+NamanDhariwal@users.noreply.github.com>
2022-11-15 22:04:16 +0530

    Add files via upload

---
+++ HANGAR/js/editor.js
@@ -0,0 +1,184 @@
+/* eslint-disable no-unused-vars */
+const editor = document.getElementById('editor');
+const loading = document.getElementById('loading');
+let userId = '';
+let userName = '';
+let dos = '';
+
+// eslint-disable-next-line no-undef
+firebase.auth().onAuthStateChanged(function(user) {
+	if (user) {
+		userId = user.uid;
+		userName = user.displayName;
+		init();
+	} else {
+		console.log(user + '' + 'logged out');
+	}
+});
+
+// editor section
+function format(command, value) {
+	document.execCommand(command, false, value);
+}
+
+function changeFont() {
+	const Font = document.getElementById('input-font').value;
+	document.execCommand('fontName', false, Font);
+}
+
+function changeSize() {
+	const size = document.getElementById('fontSize').value;
+	document.execCommand('fontSize', false, size);
+}
+
+function addDoc(word) {
+	const docId = localStorage.getItem('token');
+	// eslint-disable-next-line no-undef
+	firebase
+		.firestore()
+		.collection('docs').doc(userId).collection('documents').doc(docId).set({
+			name: userName,
+			createdAt: new Date(),
+			updated: new Date(),
+			content: word,
+		})
+		.then(() => {
+			loading.style.display = 'none';
+		})
+		.catch(function(error) {
+			console.error('Error writing document: ', error);
+		});
+}
+
+// eslint-disable-next-line no-unused-vars
+function getSingleDocDetails(docId){
+	// eslint-disable-next-line no-undef
+	firebase
+		.firestore()
+		.collection('docs')
+		.doc(userId)
+		.collection('documents')
+		.doc(docId)
+		.get()
+		.then((doc) => {
+			if (doc.exists) {
+				editor.innerHTML += doc.data().content;
+			} else {
+				console.log('No such document!');
+			}
+		}).catch(function(error) {
+			console.log('Error getting document:', error);
+		});
+}
+
+var delay = (function(){
+	var timer = 0;
+	return function(callback, ms){
+		clearTimeout (timer);
+		timer = setTimeout(callback, ms);
+	};
+})();
+
+function init(){
+	const token = localStorage.getItem('token');
+	if(!token){
+		// eslint-disable-next-line no-undef
+		const docId = firebase.firestore().collection('docs').doc(userId).collection('documents').doc().id;
+		localStorage.setItem('token', docId);
+	}else{
+		delay(function(){
+			getSingleDocDetails(token);
+		}, 1000 );
+	}
+}
+
+window.addEventListener('load', function () {
+	const log = document.getElementById('log');
+	function updateOnlineStatus() {
+		const status = document.getElementById('status');
+		editor.addEventListener('input', e => {
+			dos = e.target.innerHTML;
+			delay(function(){
+				addDoc(dos);
+			}, 1000 );
+			console.log(dos);
+			if (navigator.onLine === true) {
+				const word =	localStorage.getItem('document');
+				addDoc(word);
+				// loading.style.display = 'block';
+				status.innerHTML = 'online'.toUpperCase();
+				log.innerHTML = ' Status: ' + 'online';
+				return (status.className = 'online');
+			} else {
+				console.log('errrr2');
+				localStorage.setItem('document', dos);
+				console.log(dos);
+				status.innerHTML = 'offline'.toUpperCase();
+				log.innerHTML = ' Status: ' + 'offline';
+				return (status.className = 'offline');
+			}
+		});
+	}
+	updateOnlineStatus();
+	updateOnlineStatus();
+	window.addEventListener('online', updateOnlineStatus);
+	window.addEventListener('offline', updateOnlineStatus);
+});
+
+
+
+
+
+function apa() {
+
+	const first = document.getElementById("authorfn").textContent;
+	// const firstname = first.slice();
+	const initials = first.charAt(0);
+	document.getElementById("authorfn").innerHTML= initials + '. ,';
+
+	const dateold = document.getElementById("date").textContent;
+	const datenew = dateold.replace(',', '').replace('(', '').replace(')', '');
+	document.getElementById("date").innerHTML= datenew;
+
+	document.getElementById("authorfn").style.order = '2';
+	document.getElementById("authorln").style.order = '1';
+	document.getElementById("date").style.order = '3';
+	document.getElementById("title").style.order = '4';
+	document.getElementById("publisher").style.order = '5';
+}
+
+function mla() {
+
+	// document.getElementById("authorfn").innerHTML= firstname;
+	const dateold = document.getElementById("date").textContent;
+	const datenew = dateold.replace(',', '').replace('(', '').replace(')', '');
+	document.getElementById("date").innerHTML= datenew;
+
+	document.getElementById("authorfn").style.order = '2';
+	document.getElementById("authorln").style.order = '1';
+	document.getElementById("date").style.order = '5';
+	document.getElementById("title").style.order = '3';
+	document.getElementById("publisher").style.order = '4';
+}
+
+function chi() {
+
+	// document.getElementById("authorfn").innerHTML= first;
+
+	const dateold = document.getElementById("date").textContent;
+	const datenew = dateold.replace(',', '').replace('(', '').replace(')', '');
+	document.getElementById("date").innerHTML= '(' + datenew + '),';
+
+	document.getElementById("authorfn").style.order = '2';
+	document.getElementById("authorln").style.order = '1';
+	document.getElementById("date").style.order = '3';
+	document.getElementById("title").style.order = '4';
+	document.getElementById("publisher").style.order = '5';
+}
+
+
+$(document).read(function(){
+	$('#cmd').click(function(){
+		$('#editor').printThis();
+	})
+})
